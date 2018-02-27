95d14dd90b1a37abb8fc03d279ab0ba8caf4ccb6
nsm <smnicolas>
2018-02-27 13:15:12 -0300

    switch vs hash

--- asm/c/switch-vs-hash/hash.c
+++ asm/c/switch-vs-hash/hash.c
@@ -3,25 +3,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-typedef  void (*sum_func_t) (void*, void const *, void const *); 
-//void (*sum_funcs[14]) (void*, void const *, void const *);
-sum_func_t func_arr[22];
-func_arr[0] = sum_floats;
-sum_func_t sum_funcs[] = { sum_floats };
-//sum_funcs[1] = sum_floats;
-/* = { */
-/*      sum_floats, sum_floats, sum_floats, sum_floats, */
-/*      sum_floats, sum_floats, sum_floats, sum_floats, */
-/*      sum_floats, sum_floats, sum_floats, sum_floats, */
-/*      sum_floats, sum_floats */
-/* }; */
-
-/* void (*sum_funcs[14]) (void*, void const *, void const *) = { */
-/*      sum_floats, sum_floats, sum_floats, sum_floats, */
-/*      sum_floats, sum_floats, sum_floats, sum_floats, */
-/*      sum_floats, sum_floats, sum_floats, sum_floats, */
-/*      sum_floats, sum_floats */
-/* }; */
+void (*sum_funcs[14]) (void*, void const *, void const *) = {
+     sum_floats, sum_floats, sum_floats, sum_floats,
+     sum_floats, sum_floats, sum_floats, sum_floats,
+     sum_floats, sum_floats, sum_floats, sum_floats,
+     sum_floats, sum_floats
+};
 
 
 void sum_g(int x, void *res, void const *a, void const *b) {
