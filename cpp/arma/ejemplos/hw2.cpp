44a4f00bb943e75bfe68a7c8d620c2843cff1c63
39204 <0x9924@gmail.com>
2019-03-27 23:20:55 -0300

    latex,cpp,R

---
+++ cpp/arma/ejemplos/hw2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+#include <armadillo>
+
+int main(int argc, const char **argv) {
+
+    //arma::arma_rng::set_seed_random();
+    std::vector<double> vector{1,1,1,1,1,1,1,1,1};
+    arma::Mat<double> A(vector.data(), 3, 3);
+    std::move(vector);
+    arma::Mat<double> AtA{A * A.t()};
+    
+    std::cout << "A:" << std::endl;
+    std::cout << A << std::endl;
+
+    std::cout << "A^t A:" << std::endl;
+    std::cout << AtA << std::endl;
+
+    std::cout << "chol(A^tA):" << std::endl;
+    std::cout << chol(AtA) << std::endl;
+    return 0;
+}
---
+++ elisp/ejemplos/numeros/pares-impares.el
@@ -0,0 +1,28 @@
+(defun impares (desde hasta)
+  (interactive "ndesde: \nnhasta: ")
+  (let* ((minimo (/  desde 2))
+         (maximo (/ (1- hasta) 2))
+
+         (seq (mapcar (lambda (x) (1+ (* 2 x)))
+                      (number-sequence minimo maximo)))
+         
+         (res (reduce (lambda (x y) (format "%s, %s" x y)) seq)))
+
+    (kill-new res)
+    (message "%s" res)))
+
+
+(defun pares-reverse (desde hasta)
+  (interactive "ndesde: \nnhasta: ")
+  (let* ((minimo (/ (1+ desde) 2))
+         (maximo (/ hasta 2))
+         (seq (mapcar (lambda (x) (* 2 x))
+                      (number-sequence minimo maximo)))
+         (res (reduce (lambda (x y) (format "%s, %s" x y))
+                      (reverse seq))))
+
+    (kill-new res)
+    (message "%s" res)))
+
+
+      
